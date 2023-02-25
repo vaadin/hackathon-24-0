016f12c4d9620b95e33af08e2d5f2b2cb9c7fb6c
mukherjeesudebi <sudebi@vaadin.com>
2023-02-25 16:15:33 +0200

    added export button

--- src/main/java/com/example/application/views/spreadsheet/SpreadsheetViewSudebi.java
+++ src/main/java/com/example/application/views/spreadsheet/SpreadsheetViewSudebi.java
@@ -1,36 +1,41 @@
 package com.example.application.views.spreadsheet;
 
-import java.awt.print.PageFormat;
-import java.awt.print.PrinterException;
-import java.awt.print.PrinterJob;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.example.application.views.dialog.InvoiceDialogView;
 import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.spreadsheet.Spreadsheet;
 import com.vaadin.flow.component.spreadsheet.SpreadsheetComponentFactory;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamRegistration;
+import com.vaadin.flow.server.StreamResource;
+import com.vaadin.flow.server.VaadinSession;
 
 @Route("test")
 public class SpreadsheetViewSudebi extends VerticalLayout {
 	private Button newInvoice = new Button("Create New Invoice");
-	private Button printInvoice = new Button("PrintInvoice");
+	private Button export = new Button("Export");
 	private InvoiceDialogView dialog;
+	private Spreadsheet spreadsheet;
 
-	
 	public SpreadsheetViewSudebi() throws IOException {
 		File initialFile = new File("C:/Users/Sudebi/Desktop/shop.xlsx");
 		InputStream targetStream = new FileInputStream(initialFile);
-		
-		Spreadsheet spreadsheet = new Spreadsheet(targetStream);
+
+		spreadsheet = new Spreadsheet(targetStream);
 		spreadsheet.setHeight("400px");
 		add(spreadsheet);
 		setSizeFull();
@@ -57,27 +62,45 @@ public class SpreadsheetViewSudebi extends VerticalLayout {
 					newInvoice.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
 					return newInvoice;
 				}
+				
+				if (spreadsheet.getActiveSheetIndex() == 0 && rowIndex == 2 && columnIndex == 4) {
+					export.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+					return export;
+				}
 				return null;
 			}
 		});
-		spreadsheet.setColumnWidth(1,200);
+		spreadsheet.setColumnWidth(1, 200);
 
-		
-		
 		newInvoice.addClickListener(event -> {
-			//Notification.show("New Invoice", 3000, Position.MIDDLE);
+			// Notification.show("New Invoice", 3000, Position.MIDDLE);
 			dialog = new InvoiceDialogView();
 			dialog.setSpreadsheet(spreadsheet);
 			dialog.open();
 		});
 		
+		export.addClickListener(event -> {
+			downloadSpreadsheetFile();
+		});
 
 	}
 
-	public void createSpreadSheet() {
-		Spreadsheet spreadsheet = new Spreadsheet();
-		spreadsheet.setHeight("400px");
-		add(spreadsheet);
-		setSizeFull();
+	private Logger getLogger() {
+		return LoggerFactory.getLogger(getClass());
+	}
+
+
+	private void downloadSpreadsheetFile() {
+		try {
+			final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			spreadsheet.write(outputStream);
+			final StreamResource resource = new StreamResource("file.xlsx",
+					() -> new ByteArrayInputStream(outputStream.toByteArray()));
+			final StreamRegistration registration = VaadinSession.getCurrent().getResourceRegistry()
+					.registerResource(resource);
+			UI.getCurrent().getPage().open(registration.getResourceUri().toString());
+		} catch (Exception e) {
+			getLogger().warn("Error while processing the file to download", e);
+		}
 	}
 }
