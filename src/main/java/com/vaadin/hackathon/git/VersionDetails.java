451153444dfa2a3b1436d9b59e5c0d446661c28d
edler-san <19165931+edler-san@users.noreply.github.com>
2025-04-24 13:12:38 +0200

    Added creator to VersionDetail; changed LATEST to concrete version for Playwright

--- src/main/java/com/vaadin/hackathon/git/VersionDetails.java
+++ src/main/java/com/vaadin/hackathon/git/VersionDetails.java
@@ -6,10 +6,12 @@ import java.util.Objects;
 public class VersionDetails {
     private String version;
     private OffsetDateTime releasedOn;
+    private String creator;
 
-    public VersionDetails(final String version, final OffsetDateTime releasedOn) {
+    public VersionDetails(final String version, final OffsetDateTime releasedOn, final String creator) {
         this.version = version;
         this.releasedOn = releasedOn;
+        this.creator = creator;
     }
 
     public String getVersion() {
@@ -18,6 +20,14 @@ public class VersionDetails {
 
     public OffsetDateTime getReleasedOn() {
         return this.releasedOn;
+    }
+
+    public void setCreator(final String creator){
+        this.creator=creator;
+    }
+
+    public String getCreator(){
+        return this.creator;
     }
 
     public void setVersion(final String version) {
@@ -46,7 +56,7 @@ public class VersionDetails {
 
     @Override
     public String toString() {
-        return "VersionDetails [version=" + this.version + ", releasedOn=" + this.releasedOn + "]";
+        return "VersionDetails [version=" + this.version + ", releasedOn=" + this.releasedOn + " by " + this.creator + "]";
     }
 
 }
